1a61cbfbe2ea71af5eb8a40060626f6242fef000
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-06-21 19:27:29 +0530

    Update carousel with bigger responsive cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import InfiniteCarousel from './Components/InfiniteCarousel'
+
 
 export default function Home() {
   return (
@@ -12,6 +14,9 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <InfiniteCarousel />
+
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
