f6856869fc8815277721be035bd28b00fbc7e63b
emz95 <emma91zhang@gmail.com>
2025-06-21 23:19:15 -0700

    more spacing + font changes

--- src/app/Components/between-page.tsx
+++ src/app/Components/between-page.tsx
@@ -2,8 +2,8 @@ import InfiniteCarousel from './InfiniteCarousel'
 
 export default function BetweenPage() {
   return (
-    <div className="flex flex-col items-center justify-center p-4 sm:p-6 lg:py-40">
-      <h2 className="text-[#0078d4] text-2xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center">Our Sponsors</h2>
+    <div className="flex flex-col items-center justify-center p-4 sm:p-6 lg:py-24">
+      <h2 className="text-[#0078d4] text-4xl sm:text-3xl md:text-4xl font-semibold mb-4 sm:mb-6 md:mb-8 text-center">Our Sponsors</h2>
       <InfiniteCarousel />
     </div>
   );
