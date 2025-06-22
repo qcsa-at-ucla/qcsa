ced8a41cddce45abd9e758c2e8ed24b2022fd614
emz95 <emma91zhang@gmail.com>
2025-06-21 21:06:55 -0700

    test/stats color

--- src/app/Components/PageHead.tsx
+++ src/app/Components/PageHead.tsx
@@ -4,7 +4,8 @@ export default function PageHead(){
             <div className='text-xl sm:text-2xl md:text-3xl font-semibold m-8 sm:m-12 md:m-16'>
                 <h2>Quantum Computing Student</h2>
                 <h2>Association @UCLA</h2>
-            </div>            <div className='text-4xl sm:text-6xl md:text-7xl lg:text-8xl m-4 sm:m-6 md:m-8'>
+            </div>            
+            <div className='text-4xl sm:text-6xl md:text-7xl lg:text-8xl m-4 sm:m-6 md:m-8'>
                 <h1 className='text-[#0078d4] font-bold'>QDW2026</h1>
                 <h1 className='text-[#ffc845] font-bold'>Coming Soon!</h1>
             </div>
