0ed1604a2d57b1f825887bf427d6be20671899b3
emz95 <emma91zhang@gmail.com>
2025-06-21 22:56:51 -0700

    color change for our sponsors heading

--- src/app/Components/between-page.tsx
+++ src/app/Components/between-page.tsx
@@ -3,7 +3,7 @@ import InfiniteCarousel from './InfiniteCarousel'
 export default function BetweenPage() {
   return (
     <div className="flex flex-col items-center justify-center p-4 sm:p-6 lg:py-40">
-      <h2 className="text-blue-600 text-2xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center">Our Sponsors</h2>
+      <h2 className="text-[#0078d4] text-2xl sm:text-3xl md:text-4xl font-bold mb-4 sm:mb-6 md:mb-8 text-center">Our Sponsors</h2>
       <InfiniteCarousel />
     </div>
   );
