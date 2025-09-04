267ffd09081a48ee5b190dbb86eb0449b27ccb02
emz95 <emma91zhang@gmail.com>
2025-09-03 22:17:56 -0700

    tailwind custom colors

--- src/app/Components/MembershipForm.tsx
+++ src/app/Components/MembershipForm.tsx
@@ -3,15 +3,15 @@ export default function MembershipForm() {
   return (
   <div className="flex flex-col">
     <section className="py-16 px-4 sm:px-6 lg:px-8 bg-[#F3F8FF] flex-grow">
-      <h2 className="relative -top-4 text-[36px] font-bold text-[#234285] text-center mb-1.5">Membership</h2>
+      <h2 className="relative -top-4 text-[36px] font-bold text-main text-center mb-1.5">Membership</h2>
 
         <div className="w-full max-w-xs sm:max-w-sm md:max-w-xl lg:max-w-4xl xl:max-w-5xl mx-auto">
 
-          <form className='text-[#234285] text-2xl font-bold'>
+          <form className='text-main text-2xl font-bold'>
               {/*Name */}
               <label
                   htmlFor="name"
-                  className="block text-[#234285] font-bold mb-1"
+                  className="block text-main font-bold mb-1"
                 >
                   Name
                 </label>
@@ -30,9 +30,9 @@ export default function MembershipForm() {
                     id='first-name'
                     name='first-name'
                     className="w-full rounded-sm 
-                    border-3 border-[#234285]/40 
+                    border-3 border-main/40 
                     bg-[#F8FAFF] px-3 py-2 outline-none font-normal
-                    focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                    focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                     transition"
                   />
                 </div>
@@ -50,9 +50,9 @@ export default function MembershipForm() {
                     id='last-name'
                     name='last-name'
                     className="w-full rounded-sm 
-                    border-3 border-[#234285]/40 
+                    border-3 border-main/40 
                     bg-[#F8FAFF] px-3 py-2 outline-none font-normal
-                    focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                    focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                     transition"
                   />
                 </div>
@@ -69,9 +69,9 @@ export default function MembershipForm() {
                     id='email'
                     name='email'
                     className="w-full rounded-sm 
-                    border-3 border-[#234285]/40 
+                    border-3 border-main/40 
                     bg-[#F8FAFF] px-3 py-2 outline-none font-normal
-                    focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                    focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                     transition"
                 />
               </div>
@@ -86,9 +86,9 @@ export default function MembershipForm() {
                     id='education'
                     name='education'
                     className="w-full rounded-sm 
-                    border-3 border-[#234285]/40 
+                    border-3 border-main/40 
                     bg-[#F8FAFF] px-3 py-2 outline-none font-normal
-                    focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                    focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                     transition"
                 />
               </div>
@@ -103,9 +103,9 @@ export default function MembershipForm() {
                     id='rate'
                     name='rate'
                     className="w-full rounded-sm 
-                    border-3 border-[#234285]/40 
+                    border-3 border-main/40 
                     bg-[#F8FAFF] px-3 py-2 outline-none font-normal
-                    focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                    focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                     transition"
                 />
               </div>          
@@ -114,11 +114,11 @@ export default function MembershipForm() {
               <div className="mb-8">
                 <label
                   htmlFor="institution-name"
-                  className="block text-[#234285] font-bold mb-1"
+                  className="block text-main font-bold mb-1"
                 >
                   Institution Name
                 </label>
-                <p className="text-[#234285] font-normal mb-2">
+                <p className="text-main font-normal mb-2">
                   The most recent institution you attended
                 </p>
                 <input
@@ -126,16 +126,16 @@ export default function MembershipForm() {
                   id= 'institution-name'
                   name='institution-name'
                   className="w-full rounded-sm bg-[#F8FAFF]
-                            border-3 border-[#234285]/40 
+                            border-3 border-main/40 
                             px-3 py-2 outline-none font-normal
-                            focus:border-[#234285]/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
+                            focus:border-main/60 focus:shadow-[0_0_8px_rgba(35,66,133,0.7)]
                             transition"
                 />
               </div>
               <div className='mt-16 flex justify-center' >
                 <button
                   type = 'submit'
-                  className='rounded-sm px-12 py-3 bg-[#234285] text-white'
+                  className='rounded-sm px-12 py-3 bg-main text-white'
                 >Register</button>
               </div>
           </form>
