b58fc8ec4ae7c77982bd2c2b6a8fe601b2a38aa5
Clyde0513 <iclyde.villacrusis@gmail.com>
2025-09-08 11:51:19 -0700

    still trying to fix wcag compliance

--- src/app/Components/PageHead.tsx
+++ src/app/Components/PageHead.tsx
@@ -1,13 +1,13 @@
 export default function PageHead(){
     return (        
     <section className='text-center px-4 sm:px-6 lg:px-8'>
-            <div className='text-xl sm:text-2xl md:text-3xl font-semibold m-8 sm:m-12 md:m-16'>
-                <h2>Quantum Computing Student</h2>
-                <h2>Association @UCLA</h2>
-            </div>            
             <div className='text-4xl sm:text-6xl md:text-8xl lg:text-9xl p-16'>
                 <h1 className='text-[#0078d4] font-bold text-'>QDW2026</h1>
-                <h1 className='text-[#ffc845] font-bold'>Coming Soon!</h1>
+                <div className='text-[#ffc845] font-bold'>Coming Soon!</div>
+            </div>
+            <div className='text-xl sm:text-2xl md:text-3xl font-semibold m-8 sm:m-12 md:m-16'>
+                <h2>Quantum Computing Student</h2>
+                <div>Association @UCLA</div>
             </div>              
             <div className='m-16 flex flex-col items-center space-y-4' >
                 <p className='text-2xl text-center'>
