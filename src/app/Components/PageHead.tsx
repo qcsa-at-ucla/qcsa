f2e77970bd8c1064662c396f8aab813e87c0e616
Harshita K <harshita-cmyk@users.noreply.github.com>
2025-09-09 05:20:52 +0530

    Some changes to QDW page

--- src/app/Components/PageHead.tsx
+++ src/app/Components/PageHead.tsx
@@ -1,18 +1,10 @@
 export default function PageHead(){
     return (        
-    <section className='text-center px-4 sm:px-6 lg:px-8'>
-            <div className='text-xl sm:text-2xl md:text-3xl font-semibold m-8 sm:m-12 md:m-16'>
-                <h2>Quantum Computing Student</h2>
-                <h2>Association @UCLA</h2>
-            </div>            
+    <section className='text-center px-4 sm:px-6 lg:px-8'>          
             <div className='text-4xl sm:text-6xl md:text-8xl lg:text-9xl p-16'>
                 <h1 className='text-[#0078d4] font-bold text-'>QDW2026</h1>
                 <div className='text-[#ffc845] font-bold'>Coming Soon!</div>
-            </div>
-            <div className='text-xl sm:text-2xl md:text-3xl font-semibold m-8 sm:m-12 md:m-16'>
-                <h2>Quantum Computing Student</h2>
-                <div>Association @UCLA</div>
-            </div>              
+            </div>         
             <div className='m-16 flex flex-col items-center space-y-4' >
                 <p className='text-2xl text-center'>
                     Thank you for the amazing inaugural to our Quantum Device Workshop this year!
