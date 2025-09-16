a6263f9c378ac1ac83ba839f5bd7b3dc3dffdaab
Clyde0513 <iclyde.villacrusis@gmail.com>
2025-09-16 12:44:30 -0700

    fixed build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { Kantumruy_Pro } from "next/font/google";
 
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const kantumruy = Kantumruy_Pro({
   subsets: ["latin"],
@@ -15,10 +9,6 @@ const kantumruy = Kantumruy_Pro({
   variable: "--font-kantumruy",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "QCSA - Quantum Computing Student Association at UCLA",
