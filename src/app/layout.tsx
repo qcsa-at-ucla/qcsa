98df36c3957c850d67420932f5d7a58a61563ddf
Clyde0513 <iclyde.villacrusis@gmail.com>
2026-01-09 10:01:34 -0800

    trying to fix mobile scrolling on join page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,9 +12,6 @@ const kantumruy = Kantumruy_Pro({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  minimumScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 
