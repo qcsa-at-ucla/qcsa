cd1b5e7401b29692c3f31b064268c6c053e162e2
Clyde0513 <iclyde.villacrusis@gmail.com>
2026-02-25 17:53:55 -0800

    change to PST

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -639,7 +639,7 @@ We thank Prof. Di Luo for going in depth on his research at the intersection of 
 								{/* Embed Google Calendar. Using the public embed for quantum.ucla@gmail.com */}
 								<iframe
 									title="QCSA Event Calendar"
-									src="https://calendar.google.com/calendar/embed?src=70e5c433011f8f8b954b98c30728ec1833d5e56a7fb0602ad097bf39a302f695%40group.calendar.google.com&ctz=Europe%2FBerlin"
+									src="https://calendar.google.com/calendar/embed?src=70e5c433011f8f8b954b98c30728ec1833d5e56a7fb0602ad097bf39a302f695%40group.calendar.google.com&ctz=America%2FLos_Angeles"
 									className="w-full h-[520px] border-0"
 									loading="lazy"
 								/>
